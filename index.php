4b2cb2271a80c71e492df6cb53e93cda1558a000
aphioluiz <aphioluiz@gmail.com>
2020-03-16 21:04:39 -0300

    conflito resolvido

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "Olá Mundo, 456!";
 
-echo "Olá Aphio!";
-=======
-echo "Olá Mundo, 123!";
-
-echo "Olá Bruna!";
->>>>>>> 97d4d974d6faffd8cdcbf93c1525f89b0a4a3386
+echo "Olá Aphio e Bruna!";
 
 ?>
